33d51b2e722301379d5820a2601096f29408949a
DELL <virendesaivd77@gmail.com>
2023-03-16 17:34:13 +0530

    Shadow issue solved

--- lib/components/service_widget_vertical.dart
+++ lib/components/service_widget_vertical.dart
@@ -47,140 +47,148 @@ class ServiceWidgetVertical extends StatelessWidget {
     }
 
     Discount _discountModel = PriceConverter.discountCalculation(service);
-    return MyRippleButton(
-      onTap: () {
-        Get.toNamed(
-          RouteHelper.getServiceRoute(service.id!),
-        );
-      },
-      child: Container(
-       // margin: EdgeInsets.only(right: Dimensions.PADDING_SIZE_DEFAULT),
-        padding: const EdgeInsets.all(Dimensions.PADDING_SIZE_SMALL).copyWith(bottom: 4),
-        decoration: BoxDecoration(
-          color: Theme.of(context).cardColor,
-          borderRadius: BorderRadius.circular(Dimensions.RADIUS_SMALL),
-          boxShadow: Get.isDarkMode ? null : cardShadow,
-        ),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Stack(
-              children: [
-                ClipRRect(
-                  borderRadius: BorderRadius.all(
-                      Radius.circular(Dimensions.RADIUS_SMALL)),
-                  child: CustomImage(
-                    image:
-                        '${Get.find<SplashController>().configModel.content!.imageBaseUrl!}/service/${service.thumbnail}',
-                    fit: BoxFit.cover,
-                    width: double.maxFinite,
-                    height: Dimensions.HOME_IMAGE_SIZE,
+    return Container(
+      decoration: BoxDecoration(
+
+        // color: Theme.of(context).cardColor,
+          borderRadius: BorderRadius.circular(
+              Dimensions.RADIUS_SMALL),
+          boxShadow: Get.isDarkMode ? null : cardShadow),
+      child: MyRippleButton(
+        onTap: () {
+          Get.toNamed(
+            RouteHelper.getServiceRoute(service.id!),
+          );
+        },
+        child: Container(
+         // margin: EdgeInsets.only(right: Dimensions.PADDING_SIZE_DEFAULT),
+          padding: const EdgeInsets.all(Dimensions.PADDING_SIZE_SMALL).copyWith(bottom: 4),
+          decoration: BoxDecoration(
+            color: Theme.of(context).cardColor,
+            borderRadius: BorderRadius.circular(Dimensions.RADIUS_SMALL),
+          //  boxShadow: Get.isDarkMode ? null : cardShadow,
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Stack(
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.all(
+                        Radius.circular(Dimensions.RADIUS_SMALL)),
+                    child: CustomImage(
+                      image:
+                          '${Get.find<SplashController>().configModel.content!.imageBaseUrl!}/service/${service.thumbnail}',
+                      fit: BoxFit.cover,
+                      width: double.maxFinite,
+                      height: Dimensions.HOME_IMAGE_SIZE,
+                    ),
                   ),
-                ),
-                _discountModel.discountAmount! > 0
-                    ? Align(
-                        alignment: Alignment.topRight,
-                        child: Container(
-                          padding: EdgeInsets.all(
-                              Dimensions.PADDING_SIZE_EXTRA_SMALL),
-                          decoration: BoxDecoration(
-                            color: Theme.of(context).errorColor,
-                            borderRadius: BorderRadius.only(
-                              bottomLeft: Radius.circular(
-                                  Dimensions.RADIUS_DEFAULT),
-                              topRight: Radius.circular(
-                                  Dimensions.RADIUS_SMALL),
+                  _discountModel.discountAmount! > 0
+                      ? Align(
+                          alignment: Alignment.topRight,
+                          child: Container(
+                            padding: EdgeInsets.all(
+                                Dimensions.PADDING_SIZE_EXTRA_SMALL),
+                            decoration: BoxDecoration(
+                              color: Theme.of(context).errorColor,
+                              borderRadius: BorderRadius.only(
+                                bottomLeft: Radius.circular(
+                                    Dimensions.RADIUS_DEFAULT),
+                                topRight: Radius.circular(
+                                    Dimensions.RADIUS_SMALL),
+                              ),
+                            ),
+                            child: Text(
+                              PriceConverter.percentageOrAmount(
+                                  '${_discountModel.discountAmount}',
+                                  '${_discountModel.discountAmountType}'),
+                              style: ubuntuRegular.copyWith(
+                                  color: Theme.of(context)
+                                      .primaryColorLight),
                             ),
                           ),
-                          child: Text(
-                            PriceConverter.percentageOrAmount(
-                                '${_discountModel.discountAmount}',
-                                '${_discountModel.discountAmountType}'),
-                            style: ubuntuRegular.copyWith(
-                                color: Theme.of(context)
-                                    .primaryColorLight),
-                          ),
+                        )
+                      : SizedBox(),
+                ],
+              ),
+              SizedBox(height: 3,),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text(
+                    service.name!,
+                    style: ubuntuMedium.copyWith(
+                        fontSize: Dimensions.fontSizeSmall),
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(height: 2,),
+                  Text(
+                    service.shortDescription!,
+                    style: ubuntuLight.copyWith(
+                        fontSize: Dimensions.fontSizeExtraSmall,
+                        color: Theme.of(context).disabledColor),
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                    textAlign: TextAlign.start,
+                  ),
+                  SizedBox(height: 4,),
+                  //add to cart button
+                  if (fromType != 'fromCampaign')
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        CommonSubmitButton(
+                          text: 'Book Now'.tr,
+                          fontSize: Dimensions.fontSizeSmall,
+                          onTap:  () =>  showModalBottomSheet(
+                              useRootNavigator: true,
+                              isScrollControlled: true,
+                              backgroundColor: Colors.transparent,
+                              context: context,
+                              builder: (context) =>
+                                  ServiceCenterDialog(service: service)),
+
                         ),
-                      )
-                    : SizedBox(),
-              ],
-            ),
-            SizedBox(height: 3,),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Text(
-                  service.name!,
-                  style: ubuntuMedium.copyWith(
-                      fontSize: Dimensions.fontSizeSmall),
-                  maxLines: 2,
-                  overflow: TextOverflow.ellipsis,
-                  textAlign: TextAlign.center,
-                ),
-                SizedBox(height: 2,),
-                Text(
-                  service.shortDescription!,
-                  style: ubuntuLight.copyWith(
-                      fontSize: Dimensions.fontSizeExtraSmall,
-                      color: Theme.of(context).disabledColor),
-                  maxLines: 2,
-                  overflow: TextOverflow.ellipsis,
-                  textAlign: TextAlign.start,
-                ),
-                SizedBox(height: 4,),
-                //add to cart button
-                if (fromType != 'fromCampaign')
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      CommonSubmitButton(
-                        text: 'Book Now'.tr,
-                        fontSize: Dimensions.fontSizeSmall,
-                        onTap:  () =>  showModalBottomSheet(
-                            useRootNavigator: true,
-                            isScrollControlled: true,
-                            backgroundColor: Colors.transparent,
-                            context: context,
-                            builder: (context) =>
-                                ServiceCenterDialog(service: service)),
-
-                      ),
-                    ],
-                  ),
-                /*  Stack(
-                    children: [
-                      Align(
-                          alignment: AlignmentDirectional.bottomEnd,
-                          child: SizedBox(
-                            height: 30.0,
-                            width: 100.0,
-                            child: CustomButtonSmall(
+                      ],
+                    ),
+                  /*  Stack(
+                      children: [
+                        Align(
+                            alignment: AlignmentDirectional.bottomEnd,
+                            child: SizedBox(
+                              height: 30.0,
+                              width: 100.0,
+                              child: CustomButtonSmall(
+                                buttonText: 'Book Now'.tr,
+                              ),
+                            )),
+                        Positioned.fill(
+                          child: CustomButtonSmall(
                               buttonText: 'Book Now'.tr,
-                            ),
-                          )),
-                      Positioned.fill(
-                        child: CustomButtonSmall(
-                            buttonText: 'Book Now'.tr,
-                            onPressed: () {
-                              showModalBottomSheet(
-                                  useRootNavigator: true,
-                                  isScrollControlled: true,
-                                  backgroundColor: Colors.transparent,
-                                  context: context,
-                                  builder: (context) =>
-                                      ServiceCenterDialog(service: service));
-                            }),
-                      )
-                    ],
-                  ),*/
+                              onPressed: () {
+                                showModalBottomSheet(
+                                    useRootNavigator: true,
+                                    isScrollControlled: true,
+                                    backgroundColor: Colors.transparent,
+                                    context: context,
+                                    builder: (context) =>
+                                        ServiceCenterDialog(service: service));
+                              }),
+                        )
+                      ],
+                    ),*/
 
 
 
-              ],
-            ),
+                ],
+              ),
 
-          ],
+            ],
+          ),
         ),
       ),
     );
