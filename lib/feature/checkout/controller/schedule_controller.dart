d340e9b004b17fcc1bf404320216363862fcdee9
hpy <happypatel379@gmail.com>
2023-03-22 12:51:02 +0530

    selection of the time will be Datetime.now

--- lib/feature/checkout/controller/schedule_controller.dart
+++ lib/feature/checkout/controller/schedule_controller.dart
@@ -3,8 +3,8 @@ import 'package:repair/core/core_export.dart';
 
 class ScheduleController extends GetxController {
   ///Selected date of day
-  DateTime _selectedDate =
-      DateTime.now().add(Duration(hours: AppConstants.SCHEDULE_TIME));
+  DateTime _selectedDate = DateTime.now().add(Duration(hours: AppConstants.SCHEDULE_TIME));
+
   DateTime get selectedData => _selectedDate;
 
   ///Selected time of day
@@ -23,11 +23,7 @@ class ScheduleController extends GetxController {
 
   ///selet date function
   Future<void> selectDate() async {
-    final DateTime? picked = await showDatePicker(
-        context: Get.context!,
-        initialDate: _selectedDate,
-        firstDate: DateTime.now().subtract(Duration(days: 0)),
-        lastDate: DateTime(2101));
+    final DateTime? picked = await showDatePicker(context: Get.context!, initialDate: _selectedDate, firstDate: DateTime.now(), lastDate: DateTime(2101));
     if (picked != null) {
       _selectedDate = picked;
       update();
@@ -38,14 +34,13 @@ class ScheduleController extends GetxController {
   ///select time function
   Future<void> selectTimeOfDay() async {
     final TimeOfDay? pickedTime = await showTimePicker(
-        context: Get.context!,
-        initialTime: TimeOfDay(
-            hour: DateTime.now().hour + AppConstants.SCHEDULE_TIME,
-            minute: DateTime.now().minute));
+      context: Get.context!,
+      // initialTime: TimeOfDay(hour: DateTime.now().hour + AppConstants.SCHEDULE_TIME, minute: DateTime.now().minute),
+      initialTime: TimeOfDay(hour: DateTime.now().hour, minute: DateTime.now().minute),
+    );
 
     if (pickedTime != null) {
-      _selectedDate = DateTime(_selectedDate.year, _selectedDate.month,
-          _selectedDate.day, pickedTime.hour, pickedTime.minute);
+      _selectedDate = DateTime(_selectedDate.year, _selectedDate.month, _selectedDate.day, pickedTime.hour, pickedTime.minute);
       update();
 
       ///after receive date and time we should call buildSchedule to get time and date together
@@ -60,8 +55,7 @@ class ScheduleController extends GetxController {
   // }
 
   bool checkScheduleTime() {
-    return _selectedDate.difference(DateTime.now()) >
-        Duration(hours: AppConstants.SCHEDULE_TIME, minutes: -15);
+    return _selectedDate.difference(DateTime.now()) > Duration(hours: AppConstants.SCHEDULE_TIME, minutes: -15);
   }
 
   ///Task Task Task
